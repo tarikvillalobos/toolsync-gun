a29eb0d51ba8b6e002b5c62d6ee0513101c9d8ac
Tarik Villalobos <tarik.v@amagontech.com.br>
2026-08-27 10:45:32 -0300

    style: fit the kiosk card shadow inside its frame

--- app/aplicativo/page.js
+++ app/aplicativo/page.js
@@ -4,12 +4,12 @@ import Tela from "../components/Tela";
 export default function AppPage() {
   return (
     <Tela titulo="App">
-      {/* card fixo de 1280x800 (+1px de borda) com 32px de respiro em volta */}
+      {/* card fixo de 1280x800 (+1px de borda) com 48px de respiro pra sombra */}
       <Moldura
         titulo="App"
         src="/telas/kiosk.html"
-        largura={1346}
-        altura={866}
+        largura={1378}
+        altura={898}
         semBorda
       />
     </Tela>
